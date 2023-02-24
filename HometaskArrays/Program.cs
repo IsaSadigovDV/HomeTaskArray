4d564c99a399c7af0ae4180bd2faa94c4b3c1969
IsaSadigovDV <isans@code.edu.az>
2023-02-24 15:04:14 +0400

    Added Home Task Arrays file

---
+++ HometaskArrays/Program.cs
@@ -0,0 +1,88 @@
+﻿////Task 1:
+
+//Console.Write("Please enter your exam result:");
+//int ExamResult = int.Parse(Console.ReadLine());
+
+//if (ExamResult >= 91 && ExamResult <= 100)
+//{
+//    Console.WriteLine("Your exam score is: A");
+//}
+//else if (ExamResult >= 81 && ExamResult <= 90)
+//{
+//    Console.WriteLine("Your exam score is: B");
+//}
+//else if (ExamResult >= 71 && ExamResult <= 80)
+//{
+//    Console.WriteLine("Your exam score is: C");
+//}
+//else if (ExamResult >= 61 && ExamResult <= 70)
+//{
+//    Console.WriteLine("Your exam score is: D");
+//}
+//else if (ExamResult >= 51 && ExamResult <= 60)
+//{
+//    Console.WriteLine("Your exam score is: E");
+//}
+//else if (ExamResult <= 51)
+//{
+//    Console.WriteLine("You are failed the exam :(");
+//}
+//else
+//{
+//    Console.WriteLine("Please enter your exam result corretly!");
+//}
+
+
+
+//********************************************************************
+//Task 2: 
+
+//int[] arr = { 2, -5, 19, -1, 8, 22, -71 };
+
+//int PositiveNumbers = 0;
+//int NegativeNumbers = 0;
+
+//for (int i = 0;i < arr.Length; i++)
+//{
+//    if (arr[i] < 0)
+//    {
+//        NegativeNumbers++;
+
+//    }
+//    else if (arr[i] > 0)
+//    {
+
+//        PositiveNumbers++;
+
+//    }
+//}
+
+//Console.WriteLine("Here are negative numbers: " +  NegativeNumbers);
+
+//Console.WriteLine("Here are positive numbers: " + PositiveNumbers);
+
+
+
+//******************************************************
+////Task 3:
+
+//int[] arr = { 2, 99, 6, 25, 166, 17 };
+
+//int Biggest = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > Biggest)
+//    {
+//        Biggest = arr[i];
+//    }
+//}
+
+//Console.WriteLine(Biggest);
+
+
+
+
+
+
+
